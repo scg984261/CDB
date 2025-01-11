af7d9f1542cd2cb6f557b79c9bb69ea638680b46
scg984261 <scottgillen@hotmail.com>
2025-01-11 14:04:51 +0000

    Stored procedures to activate and deactivate customers

---
+++ StoredProcedures/ActivateCustomer.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE dbo.ActivateCustomer
+(
+	@CustomerId INT
+)
+AS
+BEGIN;
+	UPDATE dbo.Customer
+	SET IsActive = 1
+	WHERE Id = @CustomerId;
+
+	EXEC dbo.SelectCustomerById @CustomerId = @CustomerId;
+END;
---
+++ StoredProcedures/DeactivateCustomer.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE dbo.DeactivateCustomer
+(
+	@CustomerId INT
+)
+AS
+BEGIN;
+	UPDATE dbo.Customer
+	SET IsActive = 0
+	WHERE Id = @CustomerId;
+
+	EXEC dbo.SelectCustomerById @CustomerId = @CustomerId;
+END;
