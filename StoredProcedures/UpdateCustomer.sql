b3bd01d055a08e8c9ea0ddbccec0a5f71a6a9e23
scg984261 <scottgillen@hotmail.com>
2025-01-19 11:55:17 +0000

    Update Customer Stored Procedure

---
+++ StoredProcedures/UpdateCustomer.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE dbo.UpdateCustomer
+(
+	@CustomerId      INT,
+	@CompanyName     NVARCHAR(256),
+	@BusinessContact NVARCHAR(256),
+	@EmailAddress    NVARCHAR(128),
+	@ContactNumber   NVARCHAR(32),
+	@IsActive        BIT
+)
+AS
+BEGIN;
+	UPDATE dbo.Customer
+	SET CompanyName = @CompanyName,
+	BusinessContact = @BusinessContact,
+	EmailAddress = @EmailAddress,
+	ContactNumber = @ContactNumber,
+	IsActive = @IsActive
+	WHERE Id = @CustomerId;
+END;
