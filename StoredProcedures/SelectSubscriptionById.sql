308b922abba42a637e241f29c6f23aaa66e9cae0
scg984261 <scottgillen@hotmail.com>
2025-04-13 11:41:28 +0100

    Remove SubscriptionId and replace with combination of CustomerId and ServiceId

--- StoredProcedures/SelectSubscriptionById.sql
+++ StoredProcedures/SelectSubscriptionById.sql
@@ -1,16 +1,18 @@
 ﻿CREATE PROCEDURE dbo.SelectSubscriptionById
 (
-	@SubscriptionId INT
+	@CustomerId INT,
+	@ServiceId  INT
 )
 AS
 BEGIN;
-	SELECT sub.Id AS 'SubscriptionId', sub.CustomerId, sub.ServiceId, cust.CompanyName, cust.BusinessContact, serv.Name, serv.Price, serv.IsRecurring
+	SELECT cust.Id AS 'CustomerId', cust.CompanyName, cust.BusinessContact, cust.EmailAddress, cust.ContactNumber, serv.Id AS 'ServiceId', serv.Name AS 'ServiceName', serv.Price, serv.IsRecurring
 	FROM Subscription sub
 	JOIN Customer cust
 	ON cust.Id = sub.CustomerId
 	JOIN Service serv
 	ON serv.Id = sub.ServiceId
-	WHERE sub.Id = @SubscriptionId
+	WHERE cust.Id = @CustomerId
+	AND serv.Id = @ServiceId;
 END;
 
 GO
--- StoredProcedures/SelectSubscriptionsForCustomer.sql
+++ StoredProcedures/SelectSubscriptionsForCustomer.sql
@@ -4,7 +4,7 @@
 )
 AS
 BEGIN;
-	SELECT sub.Id AS 'SubscriptionId', cust.CompanyName, cust.BusinessContact, serv.Name AS 'ServiceName', serv.Price, serv.IsRecurring, sub.CreatedDateTime
+	SELECT cust.Id AS 'CustomerId', cust.CompanyName, cust.BusinessContact, serv.Id AS 'ServiceId', serv.Name AS 'ServiceName', serv.Price, serv.IsRecurring, sub.CreatedDateTime
 	FROM dbo.Subscription sub
 	JOIN dbo.Customer cust
 	ON cust.Id = sub.CustomerId
